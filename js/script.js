822f61287d9d8b53db2921c829bd6c5e0a6943b1
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-01 16:41:17 +0200

    caorusel start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("JSOK");
+/*********************************************fase 0**********************************/
+let count = [0];
+const image = document.querySelectorAll("img");
+const arrowDown = document.querySelector(".fa-chevron-down");
+const arrowUp = document.querySelector(".fa-chevron-up");
+image[count].classList.add("active");
+/*********************************************fase 1**********************************/
+//listener up
+arrowDown.addEventListener("click", function () {
+  image[count].classList.remove("active");
+  image[count++];
+  image[count].classList.add("active");
+});
+//listener down
+arrowUp.addEventListener("click", function () {
+  image[count].classList.remove("active");
+  image[count--];
+  image[count].classList.add("active");
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+# carousel immagini
+
+**fase 0**
+
+- collego il valore [0] a un index
+- collego le immagini al js
+- collego le icone al js
+- collego le immagini con un array
+
+  **fase 1**
+
+- metto un listner sulla icon-down
+- targhetto la classe del immagine con valore .legnth 0 active e cancello l'active
+- aggiungo un ++ a il valore delle immagini
+- aggiungo la classe active
