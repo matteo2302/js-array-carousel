334341a374a0469e10ef3c403ccdb9e283f4013f
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-05 14:23:24 +0200

    img + 5 or - 0 fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 console.log("JSOK");
 /*********************************************fase 0**********************************/
 let count = [0];
+const thumbnails = document.querySelector(".thumbnails");
 const arrowDown = document.querySelector(".fa-chevron-down");
 const arrowUp = document.querySelector(".fa-chevron-up");
 const carrousel = document.querySelector(".carrousel");
@@ -22,19 +23,31 @@ image[count].classList.add("active");
 arrowDown.addEventListener("click", function () {
   image[count].classList.remove("active");
   count++;
-  image[count].classList.add("active");
-  if (count > images.length - 1) {
-    image[count].classList.remove("active");
+  if (count === images.length) {
     count = 0;
-    image[count].classList.add("active");
   }
+  image[count].classList.add("active");
 });
 //listener down
 arrowUp.addEventListener("click", function () {
   image[count].classList.remove("active");
   count--;
+  if (count < 0) {
+    count = images.length - 1;
+  }
   image[count].classList.add("active");
-  // if ((count = 0)) {
-  //   count = images.length;
-  // }
 });
+/*********************************************thumbnails*************************************/
+for (let i = 0; i < images.length; i++) {
+  thumbnails.innerHTML += `<img src="${images[i]}" alt="videogame" />`;
+}
+// const imagethumb = document.querySelectorAll(".thumbnails img");
+// imagethumb[count].classList.add("bright");
+// arrowDown.addEventListener("click", function () {
+//   imagethumb[count].classList.remove("bright");
+//   count++;
+//   if (count === images.length) {
+//     count = 0;
+//   }
+//   imagethumb[count].classList.add("bright");
+// });
